2d9b37c14fc7a5b9487b41ff2584d5b9fc02077f
Vincent Tsang <vincent_tsang2017@outlook.com>
2020-03-26 23:58:37 +0800

    创建Repository
    文件加密工具，利用openssl以AES-256-CBC加密文件

---
+++ LHN4Crypt-macOS/LHN4Decrypt/mainwindow.cpp
@@ -0,0 +1,73 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QtWidgets>
+#include <QFileDialog>
+#include <QtGlobal>
+#include <QProcess>
+#include <cstdlib>
+
+QString openFile="";
+QString saveFile="";
+QString sslUtils="/usr/local/share/lhn4crypt";
+QString sslCmd=" enc -d ";
+QString sslCipher=" -aes-256-cbc ";
+QString sslMisc=" -pbkdf2";
+QString sslPwd="";
+QString finalCmd="";
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    setWindowFlags(windowFlags()& ~Qt::WindowMaximizeButtonHint);
+    setFixedSize(this->width(), this->height());
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_inputBtn_clicked()
+{
+    openFile=QFileDialog::getOpenFileName(this,"打开","～./","*.lhn");
+    openFile=openFile.replace(" ","\\ ");
+    if(openFile == ""){
+        QMessageBox::critical(this, "错误", "请先打开一个档案", QMessageBox::Yes, QMessageBox::Yes);
+    }
+    else{
+        ui->inputLabel->setText(openFile);
+    }
+}
+
+void MainWindow::on_outputBtn_clicked()
+{
+    saveFile=QFileDialog::getSaveFileName(this,"保存","~./","");
+    saveFile=saveFile.replace(" ","\\ ");
+    if(saveFile == ""){
+        QMessageBox::critical(this, "错误", "请选择档案保存位置", QMessageBox::Yes, QMessageBox::Yes);
+    }
+    else{
+        ui->outputLabel->setText(saveFile);
+    }
+}
+
+void MainWindow::on_startBtn_clicked()
+{
+    if(openFile != "" && saveFile!=""){
+        sslPwd = ui->plainTextEdit->toPlainText();
+        if(sslPwd!=""){
+            finalCmd = sslUtils + sslCmd + sslCipher + sslMisc + " -in " + openFile +" -k " + sslPwd + " -out " + saveFile;
+            //QMessageBox::information(this,"save",finalCmd);
+            const char* system_command = finalCmd.toUtf8().constData() ;
+            system(system_command);
+        }
+        else{
+            QMessageBox::critical(this, "错误", "未指定密码", QMessageBox::Yes, QMessageBox::Yes);
+        }
+    }
+    else{
+        QMessageBox::critical(this, "错误", "未指定档案输入输出位置", QMessageBox::Yes, QMessageBox::Yes);
+    }
+}
